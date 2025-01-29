80a24c86c0f3c189b04e664939e008c513e1be0b
MarkTru <bambambam228282@gmail.com>
2025-01-29 17:53:05 +0200

    another break line fix

--- frontend/src/pages/mainpage.js
+++ frontend/src/pages/mainpage.js
@@ -11,8 +11,8 @@ function MainPage() {
       <img src={Joshep_removebg_preview} alt='Joshep' height={700} width={700} className='Joshep'></img>
       <p className='desc'>Healing Magen Yoga Therapy <br></br>
 
-  Combines the ancient wisdom and traditions of numerous<br></br> lineages  of yoga and Ayurveda  with modern science in unhealthy <br></br> students & teaching yoga therapists  to skillfully assist and empower those they work<br></br> with to play an  active role  in their healing, health and well-being.  
-  <br></br> The teaching of modern yoga therapists gives them the opportunity <br></br> to work in an  integrative health and wellness environment with <br></br> groups and individuals. 
+  Combines the ancient wisdom and traditions of numerous<br></br> lineages  of yoga and Ayurveda  with modern science in unhealthy <br></br> students & teaching yoga therapists  to skillfully assist and empower <br></br> those they work with to play an  active role  in their healing, <br></br> health and well-being.  
+  The teaching of modern yoga therapists <br></br> gives them the opportunity  to work in an  integrative health <br></br> and wellness environment with  groups and individuals. 
       </p>
     </div>
   );
