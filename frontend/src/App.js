361e5841500bf5489b4c362e1ed0913dd96f6e92
MarkTru <bambambam228282@gmail.com>
2025-02-05 16:55:47 +0200

    before install prompt prevent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
+import React, { useEffect } from 'react';
 import Header from './components/header';
-import MainPage  from './pages/mainpage';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import MainPage from './pages/mainpage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CoursesPage from './pages/coursespage';
 import CoursesInfo from './pages/courseinfo';
 import ContactUs from './pages/contactus';
 
-
-
-
 function App() {
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      
+      e.preventDefault();
+      console.log('beforeinstallprompt event was prevented.');
+    };
+
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
   return (
     <Router>
       <Header />
@@ -17,7 +30,6 @@ function App() {
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/courses/:id" element={<CoursesInfo />} />
         <Route path="/contactus" element={<ContactUs />} />
-
       </Routes>
     </Router>
   );
