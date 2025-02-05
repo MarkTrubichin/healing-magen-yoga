d096df4a19e4d751ca32ed5da5ffe1926c3b4bdb
MarkTru <bambambam228282@gmail.com>
2025-02-05 18:32:46 +0200

    class names remake added facebook link and phone number

--- frontend/src/pages/contactus.js
+++ frontend/src/pages/contactus.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/contact-us.css'
 import patreon from '../images/Patreon-Logo.png';
+import facebook from '../images/facebook-icon.png';
 
 function ContactUs() {
   return (
@@ -12,13 +13,18 @@ function ContactUs() {
             To join one of our classes, please visit our portal area to register. We have a subscription covering monthly and yearly options that allow access to all our online activities, plus an option to donate per class. The donations are important to ensure we can deliver and sustain online classes in the future.
             If you would like to know more about the session or need help registering, please:
             </p>
-            <span className='takepart-patreon'>
+            <span className='takepart-sociallinks'>
                 <a href="https://patreon.com/HealingMagenYoga?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink" target='blank'>
                     <img src={ patreon } alt='patreon' width={100}></img>
                 </a>
+                <a href='https://www.facebook.com/share/1LuXDCgZct/' target='blank'>
+                    <img src={facebook} alt='facebook' width={100}></img>
+                </a>
             </span>
-            <p className='takepart-email'>
+            <p className='takepart-contacts'>
             healingmagenyoga@gmail.com
+            <br></br>
+            tel.: +972 542078839
             </p>
             
         </div>
